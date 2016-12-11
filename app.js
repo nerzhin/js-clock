9680a86a194f8f0b560749b85089d63bb8f4cf5d
nerzhin <grach.scheglov@gmail.com>
2016-12-11 23:44:00 +0200

    minor changes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 function getHourDeg(hours, minutes, seconds) {
-  return ((3600*hours + 60*minutes + seconds) / 43200)*360 +360/4;
+  let hourHand = document.querySelector('.hour-hand');
+ if (( ((3600*hours + 60*minutes + seconds) / 43200)*360 + 90 )== 90) {
+   hourHand.style.transition = '0';
+ };
+  return ((3600*hours + 60*minutes + seconds) / 43200)*360 + 90;
 }
 
 function getMinuteDeg(hours, minutes, seconds)  {
-  return ((60*minutes+seconds)/3600)*360 + 360/4;
+  let minuteHand = document.querySelector('.minute-hand');
+  if ((((60*minutes+seconds)/3600)*360 + 90) == 90) {
+    minuteHand.style.transition = '0';
+  };
+  return ((60*minutes+seconds)/3600)*360 + 90;
 }
 
 function getSecondDeg(hours, minutes, seconds) {
-  return (seconds/60)*360 + 360/4;
+  let secondHand = document.querySelector('.second-hand');
+  if (((seconds/60)*360 + 90) == 90) {
+    secondHand.style.transition = '0';
+  };
+  return (seconds/60)*360 + 90;
+
 }
 
 function getTime() {
@@ -23,6 +36,8 @@ let secondHand = document.querySelector('.second-hand');
 hourHand.style.transform = `rotate(${getHourDeg(hours, minutes, seconds)}deg)`;
 minuteHand.style.transform = `rotate(${getMinuteDeg(hours, minutes, seconds)}deg)`;
 secondHand.style.transform = `rotate(${getSecondDeg(hours, minutes, seconds)}deg)`;
+console.log(getHourDeg(hours, minutes, seconds), getMinuteDeg(hours, minutes, seconds), getSecondDeg(hours, minutes, seconds));
 }
 
+getTime();
 setInterval(getTime, 1000);
